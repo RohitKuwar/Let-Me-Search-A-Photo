8301913883dc290e2c72ed66f28187d7c199e924
RohitKuwar <rohit1kuwar@gmail.com>
2022-01-03 15:12:12 +0530

    added like and download feature

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -1,7 +1,30 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDownload } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-solid-svg-icons'
 import styles from './image.module.css'
 
 const Image = ({ imageDetails }) => {
+  const [click, setClick] = useState(false)
+  const toggle = () => {
+    return click ? setClick(false) : setClick(true)
+  }
+  const download = () => {
+    axios({
+      url: imageDetails.urls.regular,
+      method: "GET",
+      responseType: "blob"
+    }).then(response => {
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement("a");
+      link.href = url;
+      link.setAttribute("download", `${imageDetails.id}.jpg`);
+      document.body.appendChild(link);
+      link.click();
+    })
+  }
+
   return (
     <li key={imageDetails.id} className={styles.imageContainer}>
       <img 
@@ -11,7 +34,12 @@ const Image = ({ imageDetails }) => {
         alt={imageDetails.alt_description}
       />
       <div className={styles.overlay}>
-        <span>&#10084; {imageDetails.likes}</span>
+        <span><FontAwesomeIcon icon={faHeart} size="lg" style={{color: click ? 'red' : 'white'}} onClick={toggle} />
+          {click ? imageDetails.likes + 1 : imageDetails.likes}
+        </span>
+        <span>
+          <FontAwesomeIcon icon={faDownload} size="lg" onClick={download} />
+        </span>
       </div>
     </li>
   );
