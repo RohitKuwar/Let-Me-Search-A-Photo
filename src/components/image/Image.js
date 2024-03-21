6f5569d5473ab685d46ef90e52fb96a59d5e1c8d
RohitKuwar <rohit1kuwar@gmail.com>
2024-03-22 00:06:38 +0530

    layout changes

--- src/components/image/Image.js
+++ src/components/image/Image.js
@@ -1,51 +1,110 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faDownload } from '@fortawesome/free-solid-svg-icons'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
-import styles from './image.module.css'
+import styles from "./image.module.css";
+import iconDownload from "../../assets/arrow-down-solid.svg";
+import iconLike from "../../assets/heart-solid-red.svg";
+import iconUnlike from "../../assets/heart-solid.svg";
 
 const Image = ({ imageDetails }) => {
-  const [click, setClick] = useState(false)
-  
-  const toggle = () => {
-    return click ? setClick(false) : setClick(true)
-  }
+  const [click, setClick] = useState(false);
 
   const download = () => {
     axios({
       url: imageDetails.urls.regular,
       method: "GET",
-      responseType: "blob"
-    }).then(response => {
+      responseType: "blob",
+    }).then((response) => {
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
       link.setAttribute("download", `${imageDetails.id}.jpg`);
       document.body.appendChild(link);
       link.click();
-    })
-  }
+    });
+  };
 
   return (
-    <li key={imageDetails.id} className={styles.imageContainer}>
-      <img 
-        width="400" 
-        src={imageDetails.urls.regular} 
-        key={imageDetails.id} 
-        alt={imageDetails.alt_description}
-        loading="lazy"
-      />
-      <div className={styles.overlay}>
-        <span>
-          <FontAwesomeIcon icon={faHeart} size="lg" style={{color: click ? 'red' : 'white'}} onClick={toggle} />
-          &nbsp;
-          {click ? imageDetails.likes + 1 : imageDetails.likes}
-        </span>
-        <span>
-          <FontAwesomeIcon icon={faDownload} size="lg" onClick={download} />
-        </span>
-      </div>
+    <li
+      key={imageDetails.id}
+      className={styles.imageContainer}
+      title={imageDetails.alt_description}
+    >
+      {/* <a href={imageDetails.links.download} target="_blank" rel="noreferrer"> */}
+        <img
+          width="400"
+          src={imageDetails.urls.regular}
+          key={imageDetails.id}
+          alt={imageDetails.alt_description}
+          loading="lazy"
+        />
+        <div className={styles.overlay}>
+          <div style={{ display: "flex", justifyContent: "flex-end" }}>
+            <div
+              title="like this image"
+              style={{
+                backgroundColor: "#ffffffe6",
+                border: "none",
+                width: "22px",
+                height: "22px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "8px",
+                cursor: 'pointer'
+              }}
+              onClick={() => setClick(!click)}
+            >
+              <img
+                src={click ? iconLike : iconUnlike}
+                style={{ width: "18px", height: "18px" }}
+                alt="like unlike icon"
+              />
+            </div>
+          </div>
+          <div style={{ display: "flex", justifyContent: "space-between" }}>
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <a
+                href={imageDetails.user.portfolio_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src={imageDetails.user.profile_image.medium}
+                  alt={imageDetails.user.name}
+                  style={{
+                    width: "30px",
+                    height: "30px",
+                    border: "none",
+                    borderRadius: "50%",
+                  }}
+                />
+              </a>
+              <span>{imageDetails.user.name}</span>
+            </div>
+            <div
+              title="download this image"
+              style={{
+                backgroundColor: "#ffffffe6",
+                border: "none",
+                width: "22px",
+                height: "22px",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "8px",
+                cursor: 'pointer'
+              }}
+            >
+              <img
+                src={iconDownload}
+                style={{ width: "18px", height: "18px" }}
+                onClick={download}
+                alt="download icon"
+              />
+            </div>
+          </div>
+        </div>
+      {/* </a> */}
     </li>
   );
 };
