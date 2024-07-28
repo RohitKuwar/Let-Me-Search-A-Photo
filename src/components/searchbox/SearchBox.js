60a7a89680f85a9d5b972bff92cac15524d579b4
RohitKuwar <rohit1kuwar@gmail.com>
2024-07-28 16:19:40 +0530

    added debounce function

--- src/components/searchbox/SearchBox.js
+++ src/components/searchbox/SearchBox.js
@@ -3,14 +3,13 @@ import styles from './searchbox.module.css'
 
 const SearchBox = ({ search, setSearch }) => {
   return (
-    <div className={styles.searchContainer}>
       <input
         type="text"
         value={search}
         placeholder="Search photos"
         onChange={(e) => setSearch(e.target.value)}
+        className={styles.searchContainer}
       />
-    </div>
   );
 };
 
